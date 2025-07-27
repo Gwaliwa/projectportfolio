74a464d62574c1b24452bf08bfcf063e8138667e
gmashaka <gwaliwa2010@gmail.com>
2025-07-26 20:41:11 -0400

    add column space

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(layout="wide")
 
-col1, col2 = st.columns(2)
+col1, col_empty, col2 = st.columns([1.5,0.5,1.5])
 
 with col1:
     st.image("images/photo.png")
@@ -16,3 +17,24 @@ with col2:
     """
     #st.write(content)
     st.info(content)
+
+content2= """ Below are some apps i build on python feel free to contact me
+    """
+st.write(content2)
+
+df = pd.read_csv("data.csv", sep=(";"))
+col3, col4 = st.columns(2)
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(df["title"][index])
+        st.write(row["description"])
+        st.image("images/"+ row['image'])
+        st.write(f"source: {row['url']}")
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(df["title"][index])
+        st.write(row["description"])
+        st.image("images/"+ row['image'])
+        st.write(f"source: {row['url']}")
