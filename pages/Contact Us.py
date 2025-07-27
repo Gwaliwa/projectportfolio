f635f3803a390f7f308d266f5bdaf992fb0aa413
gmashaka <gwaliwa2010@gmail.com>
2025-07-27 10:44:04 -0400

    email add

--- pages/Contact Us.py
+++ pages/Contact Us.py
@@ -5,8 +5,12 @@ st.header("Contact Us")
 
 with st.form(key="my_form"):
     user_email = st.text_input("Enter your email")
-    message = st.text_area("Enter your message")
-    message = message + "\n" + user_email
+    raw_message = st.text_area("Text")
+    message = f"""\
+Subject: New email from {user_email}
+From: {user_email}
+{raw_message}
+"""
     submit = st.form_submit_button(label="Submit")
 
     if submit:
