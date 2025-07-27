2de88b617b44d55a0fb9af3f45ab160e3b8254fb
gmashaka <gwaliwa2010@gmail.com>
2025-07-27 09:00:03 -0400

    email add

--- send_email.py
+++ send_email.py
@@ -11,12 +11,6 @@ def send_email(message):
     receiver = "gwaliwa10@gmail.com"
     context = ssl.create_default_context()
 
-    message= """\
-    Subject:
-    Hi!
-    How are you?
-    
-    """
 
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)
